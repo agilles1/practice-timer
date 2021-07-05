f54dbce6435e38504b44b0f769eec95eadd25c4a
agilles1 <adam.g.tpt@gmail.com>
2021-07-05 15:00:42 -0400

    Add TimerContainer

---
+++ src/containers/TimerContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TimerContainer extends Component {
+    state = {  }
+    render() { 
+        return (
+            <h1>This is the app</h1>
+        );
+    }
+}
+ 
+export default TimerContainer;
