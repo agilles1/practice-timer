9f1f49d6452a6eb70ceaef044f64ffbd2adf350b
agilles1 <adam.g.tpt@gmail.com>
2021-07-05 16:36:34 -0400

    Countdown timer added

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,55 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-// function Timer() {
+export default function Timer(props) {
 
-//     const [seconds, setSeconds] = useState(50)
+    const [seconds, setSeconds] = useState(props? 0:props)
 
-//     const startTimer = () => {
-//         setInterval(setSeconds(seconds - 1), 1000)
-//     }
+    function startTimer() {
+        const interval = setInterval(() => {
+            setSeconds(seconds => seconds - 1);
+          }, 1000);
+          return () => clearInterval(interval);
+    }
     
-//     return (
-//         <div>
-//             {seconds}
-//             <button onClick={startTimer}>+</button>
-//         </div>
-//     )
-// }
-
-// export default Timer
-
-const Timer = (props) => {
-    const {initialMinute = 0,initialSeconds = 15} = props;
-    const [ minutes, setMinutes ] = useState(initialMinute);
-    const [seconds, setSeconds ] =  useState(initialSeconds);
-    
-    useEffect(()=>{
-    let myInterval = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(myInterval)
-                } else {
-                    setMinutes(minutes - 1);
-                    setSeconds(59);
-                }
-            } 
-        }, 1000)
-        return ()=> {
-            clearInterval(myInterval);
-          };
-    });
-
     return (
         <div>
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
+            {seconds}
+            <button onClick={startTimer}>+</button>
         </div>
     )
 }
 
-export default Timer;
+
+// const Timer = (props) => {
+//     const {initialMinute = 0,initialSeconds = 15} = props;
+//     const [ minutes, setMinutes ] = useState(initialMinute);
+//     const [seconds, setSeconds ] =  useState(initialSeconds);
+    
+//     useEffect(()=>{
+//     let myInterval = setInterval(() => {
+//             if (seconds > 0) {
+//                 setSeconds(seconds - 1);
+//             }
+//             if (seconds === 0) {
+//                 if (minutes === 0) {
+//                     clearInterval(myInterval)
+//                 } else {
+//                     setMinutes(minutes - 1);
+//                     setSeconds(59);
+//                 }
+//             } 
+//         }, 1000)
+//         return ()=> {
+//             clearInterval(myInterval);
+//           };
+//     });
+
+//     return (
+//         <div>
+//         { minutes === 0 && seconds === 0
+//             ? null
+//             : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+//         }
+//         </div>
+//     )
+// }
+
+// export default Timer;
